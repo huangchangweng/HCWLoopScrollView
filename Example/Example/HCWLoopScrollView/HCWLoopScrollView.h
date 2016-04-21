41aaa604b256ce7f9afcdb0183c58685550d0aca
huangchangweng <599139419@qq.com>
2016-04-21 15:55:03 +0800

    2

---
+++ Example/Example/HCWLoopScrollView/HCWLoopScrollView.h
@@ -0,0 +1,13 @@
+//
+//  HCWLoopScrollView.h
+//  Example
+//
+//  Created by HCW on 16/4/21.
+//  Copyright © 2016年 HCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCWLoopScrollView : UIView
+
+@end
---
+++ Example/Example/HCWLoopScrollView/HCWLoopScrollView.m
@@ -0,0 +1,21 @@
+//
+//  HCWLoopScrollView.m
+//  Example
+//
+//  Created by HCW on 16/4/21.
+//  Copyright © 2016年 HCW. All rights reserved.
+//
+
+#import "HCWLoopScrollView.h"
+
+@implementation HCWLoopScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
